7974269034b7958df323a9554dca07f5feed87f4
Alek Dembowski <alek@lightpegasus.com>
2015-12-03 22:31:56 -0500

    Created day 3

--- adventofcode.go
+++ adventofcode.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Printf("Day1: %v, %v\n", NotQuiteLisp1(), NotQuiteLisp2())
 	fmt.Printf("Day2: %v, %v\n", NoMath1(), NoMath2())
-
+	fmt.Printf("Day3: %v, %v\n", GridWorld1(), GridWorld2())
 }
